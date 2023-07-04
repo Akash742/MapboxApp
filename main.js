e2190efa9415a1ebee2714d8b850700f7f79b293
Akash742 <akashdey742@gmail.com>
2023-07-04 18:30:55 +0530

    main.js is introduced in app

---
+++ main.js
@@ -0,0 +1,19 @@
+import { setupMap, renderVector } from "./script.js";
+
+// Turn component with given id into mapbox 
+// Use the given COG server url for rendering the raster layers
+setupMap("map", "https://cog-nk.kesowa.com");
+
+// Render all given geojson urls
+// Center on first geojson url
+renderVector([
+  "https://cdn-dev.kesowa.com/vector/00854a1e-568d-42db-84e9-a310df7593c9.geojson",
+  "https://cdn-dev.kesowa.com/vector/59af3119-fd68-48d0-966a-9a0008ec2b18.geojson"
+])
+
+// Render all given ortho urls
+// Center on first raster url
+/*renderRaster([
+  "https://cdn-dev.kesowa.com/raster/a0789482-62c7-4ab5-b9a4-9f83694d0a9e.tif",
+  "https://cdn-dev.kesowa.com/raster/f6b443a1-7724-4bd6-9be9-4f33fd0fc98c.tif"
+])*/
--- script.js
+++ script.js
@@ -1,109 +1,123 @@
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ"; // Replace with your Mapbox access token
+let map;
+let cogServerUrl
+export function setupMap(containerId, serverUrl){
 
-const map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11", // Replace with your preferred map style
-  center: [-74.5, 40], // Replace with your desired initial center coordinates
-  zoom: 9, // Replace with your desired initial zoom level
-});
-
-//Add pane control
-map.on("load", () => {
-  const pane = document.getElementById("pane");
-  const layerRadios = pane.querySelectorAll('input[type="radio"]');
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibmVlbGR1dHRhMTkiLCJhIjoiY2tweG9mN3F4MThrNTJ4cDk0enVjcTN4dCJ9.uxa_h0rjqumTxFMI1QELKQ"; // Replace with your Mapbox access token
   
-  layerRadios.forEach((radio) => {
-    radio.addEventListener("change", () => {
-      const selectedLayer = radio.value;
-      map.setLayoutProperty("layer", "visibility", "none");
-      map.setLayoutProperty(selectedLayer, "visibility", "visible");
-    });
-  });
-})
-
-// Add raster layer
-/*map.on("load", () => {
-  map.addSource("raster-source", {
-    type: "raster",
-    tiles: ["PATH_TO_RASTER_TILES/{z}/{x}/{y}.png"], // Replace with the path to your raster tiles
-    tileSize: 256,
+  cogServerUrl = serverUrl;
+  
+  map = new mapboxgl.Map({
+    container: containerId,
+    style: "mapbox://styles/mapbox/streets-v11", // Replace with your preferred map style
+    center: [-74.5, 40], // Replace with your desired initial center coordinates
+    zoom: 9, // Replace with your desired initial zoom level
   });
 
-  map.addLayer({
-    id: "raster-layer",
-    type: "raster",
-    source: "raster-source",
-    paint: {},
+  // Add zoom controls
+  const zoomControls = new mapboxgl.NavigationControl();
+  map.addControl(zoomControls, 'top-right');
+}
+
+// Add raster layer from GeoJSON
+/*export function renderRaster(geojsonFilePaths) {
+  geojsonFilePaths.forEach((geojsonFilePath) => {
+    fetch(geojsonFilePath)
+      .then((response) => response.json())
+      .then(parsedGeojson => {
+        const rasterSourceId = `raster-source-${geojsonFilePath}`;
+        const rasterLayerId = `raster-layer-${geojsonFilePath}`;
+
+        // Add the raster source
+        map.addSource(rasterSourceId, {
+          type: "raster",
+          tiles: [`${cogServerUrl}/${geojsonFilePath}/{z}/{x}/{y}`], // Replace with the path to your raster tiles
+          tileSize: 256,
+        });
+
+        // Add the raster layer
+        map.addLayer({
+          id: rasterLayerId,
+          type: "raster",
+          source: rasterSourceId,
+          paint: {
+            // Apply your desired paint properties here
+            "raster-opacity": 0.7,
+            "raster-hue-rotate": 180,
+            // Add more paint properties as needed
+          },
+        });
+      })
+      .catch((error) => {
+        console.error("Error loading GeoJSON file:", error);
+      });
   });
-});*/
+}*/
+
 
 // Add vector layer from GeoJSON
-map.on("load", () => {
-  const geojsonFilePath = "./assets/WaterBody_&_Road/AA-II_Street.geojson"; // Replace with the path to your GeoJSON file
-  
-  fetch(geojsonFilePath)
-    .then(response => response.json())
-    .then(parsedGeojson => {
-      // Center the map on the points using Turf.js
-      const pointFeatures = parsedGeojson.features.filter(feature => feature.geometry.type === "MultiPolygon");
-      const center = turf.centerOfMass(turf.featureCollection(pointFeatures));
-      const [lng, lat] = center.geometry.coordinates;
+export function renderVector(geojsonFilePaths) {
+  geojsonFilePaths.forEach(geojsonFilePath => {
+    fetch(geojsonFilePath)
+      .then(response => response.json())
+      .then(parsedGeojson => {
 
-      map.setCenter([lng, lat]);
-      map.setZoom(12); // Adjust the zoom level as needed
+        // Center the map on the points using Turf.js
+        const pointFeatures = parsedGeojson.features.filter(feature => feature.geometry.type === "MultiPolygon");
+        const center = turf.centerOfMass(turf.featureCollection(pointFeatures));
+        const [lng, lat] = center.geometry.coordinates;
 
-      map.addSource("vector-source", {
-        type: "geojson",
-        data: parsedGeojson,
+        map.setCenter([lng, lat]);
+        map.setZoom(12); // Adjust the zoom level as needed
+
+        map.addSource(geojsonFilePath, {
+          type: "geojson",
+          data: parsedGeojson,
+        });
+
+        map.addLayer({
+          id: `vector-layer-${geojsonFilePath}`,
+          type: "fill",
+          source: geojsonFilePath,
+          paint: {
+            'fill-color': ['coalesce', ['get', 'fill']],
+            'fill-opacity': ['coalesce', ['get', 'fill-opacity'], 0.3]
+          },
+          filter: ['==', ['geometry-type'], 'Polygon']
+        });
+
+        map.addLayer({
+          id: `vector-layer-outline-${geojsonFilePath}`,
+          type: "line",
+          source: geojsonFilePath,
+          paint: {
+            'line-color': ['coalesce', ['get', 'stroke']],
+            'line-width': ['coalesce', ['get', 'stroke-width'], 2],
+            'line-opacity': ['coalesce', ['get', 'stroke-opacity'], 1]
+          },
+          filter: ['==', ['geometry-type'], 'Polygon']
+        });
+
+        map.addLayer({
+          id: `vector-layer-line-${geojsonFilePath}`,
+          type: "line",
+          source: geojsonFilePath,
+          paint: {
+            'line-color': ['coalesce', ['get', 'stroke']],
+            'line-width': ['coalesce', ['get', 'stroke-width'], 2],
+            'line-opacity': ['coalesce', ['get', 'stroke-opacity'], 1]
+          },
+          filter: ['==', ['geometry-type'], 'LineString']
+        });
+        //Marker
+        const marker = new mapboxgl.Marker()
+        .setLngLat([lng, lat]) // Replace [lng, lat] with the desired marker coordinates
+        .addTo(map)
+      })
+      .catch(error => {
+        console.error("Error loading GeoJSON file:", error);
       });
-
-      map.addLayer({
-        id: 'map-data-fill',
-        type: 'fill',
-        source: 'vector-source',
-        paint: {
-          'fill-color': ['coalesce', ['get', 'fill']],
-          'fill-opacity': ['coalesce', ['get', 'fill-opacity'], 0.3]
-        },
-        filter: ['==', ['geometry-type'], 'Polygon']
-      });
-
-      map.addLayer({
-        id: 'map-data-fill-outline',
-        type: 'line',
-        source: 'vector-source',
-        paint: {
-          'line-color': ['coalesce', ['get', 'stroke']],
-          'line-width': ['coalesce', ['get', 'stroke-width'], 2],
-          'line-opacity': ['coalesce', ['get', 'stroke-opacity'], 1]
-        },
-        filter: ['==', ['geometry-type'], 'Polygon']
-      });
-
-      map.addLayer({
-        id: 'map-data-line',
-        type: 'line',
-        source: 'vector-source',
-        paint: {
-          'line-color': ['coalesce', ['get', 'stroke']],
-          'line-width': ['coalesce', ['get', 'stroke-width'], 2],
-          'line-opacity': ['coalesce', ['get', 'stroke-opacity'], 1]
-        },
-        filter: ['==', ['geometry-type'], 'LineString']
-      });
-    })
-    .catch(error => {
-      console.error("Error loading GeoJSON file:", error);
     });
+    
+}
 
-//Marker
-const marker = new mapboxgl.Marker()
-.setLngLat([88.393462299414097, 22.593231705561085, -196447.000000000029104]) // Replace [lng, lat] with the desired marker coordinates
-.addTo(map);
-});
-
-
-// Add zoom controls
-const zoomControls = new mapboxgl.NavigationControl();
-map.addControl(zoomControls, 'top-right');
